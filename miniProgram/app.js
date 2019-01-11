815d6895adc46bac3f8627cf6aae4212a1dc1322
yong <zengjunyong@gmail.com>
2019-01-11 16:18:55 +0800

    从后台跳到前台时，重新进入roomlist

--- miniProgram/app.js
+++ miniProgram/app.js
@@ -1,13 +1,17 @@
 //app.js
 
 App({
-  onLaunch: function () {
-    console.log("App Launch");
-  },
-  onShow: function () {
-    console.log("App Show");
-  },
-  onHide: function () {
-    console.log("App Hide");
-  },
+    onLaunch: function () {
+        console.log("App Launch");
+    },
+    onShow: function () {
+        console.log("App Show");
+        let url = '../roomlist/roomlist';
+        wx.navigateTo({
+            url
+        });
+    },
+    onHide: function () {
+        console.log("App Hide");
+    },
 })
